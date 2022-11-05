c0f40305c3856b438859f74f4b4e405951c17d6b
Jan Weidner <jw3126@gmail.com>
2022-11-05 21:29:19 +0100

    init

--- src/MutationChecks.jl
+++ src/MutationChecks.jl
@@ -1,5 +1,135 @@
 module MutationChecks
 
-# Write your package code here.
+struct Call{F,Args,KW <: NamedTuple}
+    f::F
+    args::Args
+    kw::KW
+end
+function make_call(f, args...; kw...)
+    F = typeof(f)
+    Args = typeof(args)
+    kw_ = NamedTuple(kw)
+    KW = typeof(kw_)
+    Call{F,Args,KW}(f,args,kw_)
+end
+
+Base.@kwdef struct Options
+    cmp = isequal
+    copy = deepcopy
+end
+
+
+function execute(c::Call)
+    c.f(c.args...; c.kw...)
+end
+
+function trymutcheck(call::Call, options::Options, expr)
+    call_backup = options.copy(call)::typeof(call)
+    out = execute(call)
+    res = MutationResults(call, call_backup, options)
+    if ispass(res)
+        return (out, nothing)
+    end
+    res_self_check = MutationResults(call_backup, deepcopy(call_backup), options)
+    if ispass(res_self_check)
+        return (out, MutCheckFail(res, expr))
+    else
+        return (out, CmpSelfCheckFail(res_self_check, expr))
+    end
+end
+
+macro mutcheck(code)
+    mutcheckmacro(code)
+end
+
+function mutcheckmacro(code)
+    if Meta.isexpr(code, :do)
+        error("do blocks are currently not supported. Got $code")
+    end
+    if !Meta.isexpr(code, :call)
+        error("Expected a function call, got $code instead.")
+    end
+    call = if length(code.args) >= 2 && Meta.isexpr(code.args[2], :parameters)
+        Expr(:call, make_call, code.args[2], code.args[1], code.args[3:end]...)
+    else
+        Expr(:call, make_call, code.args...)
+    end
+    options = Options()
+    quote
+        out, err = $trymutcheck($(esc(call)), $options, $(QuoteNode(code)))
+        if err isa Exception
+            throw(err)
+        else
+            out
+        end
+    end
+end
+
+struct MutationResults
+    f_differs::Bool
+    differ_args::Vector{Int}
+    differ_kw::Vector{Symbol}
+    call1::Call
+    call2::Call
+    options::Options
+end
+
+struct CmpSelfCheckFail <: Exception
+    result::MutationResults
+    expr
+end
+
+
+struct MutCheckFail <: Exception
+    result::MutationResults
+    expr
+end
+function _showerror(io, err::Union{CmpSelfCheckFail, MutCheckFail})
+    if err isa CmpSelfCheckFail
+        println(io, "Self comparison failed.")
+    else
+        @assert err isa MutCheckFail
+        println(io, "Mutation detected.")
+    end
+    res = err.result
+    msg_pos = if isempty(res.differ_args)
+        "None were mutated."
+    else
+        "Mutated positions are $(res.differ_args)"
+    end
+    msg_kw = if isempty(res.differ_args)
+        "None were mutated."
+    else
+        "Mutated keywords are $(res.differ_kw)"
+    end
+    msg = """
+    * Expression:           $(err.expr)
+    * Calle mutated:        $(res.f_differs)
+    * Positional arguments: $(msg_pos)
+    * Keyword arguments:    $(msg_kw)
+    """
+    println(io, msg)
+end
+Base.showerror(io::IO, err::CmpSelfCheckFail) = _showerror(io, err)
+Base.showerror(io::IO, err::MutCheckFail) = _showerror(io, err)
+
+function ispass(o::MutationResults)
+    isempty(o.differ_args) && isempty(o.differ_kw) && (!o.f_differs)
+end
+
+function MutationResults(call1::Call, call2::Call, options::Options)
+    f_differs = !options.cmp(call1.f, call2.f)::Bool
+    differ_args = filter(eachindex(call1.args, call2.args)) do i
+        arg1 = call1.args[i]
+        arg2 = call2.args[i]
+        !options.cmp(arg1, arg2)::Bool
+    end
+    differ_kw = filter(collect(propertynames(call1.kw))) do s
+        kw1 = call1.kw[s]
+        kw2 = call2.kw[s]
+        !options.cmp(kw1, kw2)::Bool
+    end
+    MutationResults(f_differs, differ_args, differ_kw, call1, call2, options)
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,48 @@
-using MutationChecks
+using MutationChecks: @mutcheck, MutCheckFail, CmpSelfCheckFail
 using Test
 
+pass(args...; ret, kw...) = ret
+apply(f, args...; kw...) = f(args...; kw...)
+
+struct Pass end
+(::Pass)(args...; ret, kw...) = ret
+
+struct Ev{T}
+    value::T
+end
+Base.:(==)(o1::Ev, o2::Ev) = o1.value == o2.value
+function (o::Ev)(f, args...; kw...)
+    f(o.value)
+end
+
+mutable struct MutContainer
+    value
+end
 @testset "MutationChecks.jl" begin
-    # Write your tests here.
+    x = 1
+    y = 2
+    @test sin(10) == @mutcheck sin(10)
+    @test nothing === @mutcheck Nothing()
+    @test 20 === @mutcheck pass(;ret=20)
+    foo = (a=10,b=20)
+    a = (1,2,3)
+    @test 10 === @mutcheck pass(x,y,a..., c=3; foo..., ret=10)
+    @test 11 === @mutcheck pass(x->x,y,c=3; foo..., ret=11)
+
+    copykw!(;src, dst) = copy!(dst, src)
+    @test_throws MutCheckFail @mutcheck copykw!(src=[1], dst=[2])
+
+    x = [1]
+    y = [2]
+    @test_throws MutCheckFail @mutcheck copy!(x,y)
+    @test_throws MutCheckFail @mutcheck apply(empty!, [1])
+
+    @test Ev([1]) == deepcopy(Ev([1]))
+    @test_throws MutCheckFail @mutcheck Ev([1])(empty!)
+    @test_throws MutCheckFail @mutcheck Ev([1])(empty!, 1,2,3, a..., x=2, )
+    @test !isequal(MutContainer(1), MutContainer(1))
+    @test !isequal(MutContainer(MutContainer(1)), MutContainer(MutContainer(1)))
+
+    @test_throws CmpSelfCheckFail @mutcheck pass(MutContainer(1), ret=1)
+    @test_throws CmpSelfCheckFail @mutcheck pass(MutContainer(MutContainer(1)), ret=1)
 end
