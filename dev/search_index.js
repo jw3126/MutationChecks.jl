ac38f6e27c129653d190e552fc0a5a40266b94ef
Documenter.jl <documenter@juliadocs.github.io>
2022-11-05 20:32:12 +0000

    build based on c0f4030

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MutationChecks","category":"page"},{"location":"#MutationChecks","page":"Home","title":"MutationChecks","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MutationChecks.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MutationChecks]","category":"page"}]
+}
